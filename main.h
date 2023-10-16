456736fb1b4deab6f3fa0393430e857a86b71ed9
Montassar Hajri <89592438+MontaCoder@users.noreply.github.com>
2023-10-16 20:36:53 +0100

    Update main.h

--- main.h
+++ main.h
@@ -4,10 +4,11 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
-<<<<<<< HEAD
+
+int _strlenc(const char *s);
+int _strlen(char *s);
 int printf_string(va_list val);
-=======
+int printf_37(void);
 int _printf(const char *format, ...);
 int printf_char(va_list val);
->>>>>>> 2c09ab26cd4c7cd3b3b026cd1956ee267bfa6742
 #endif
