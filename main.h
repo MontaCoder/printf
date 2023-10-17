94330738f264ab96574e9de6b977d69c479ca0bd
Montassar <@outlook.com>
2023-10-17 09:13:55 -0700

    printf str to ROT13 place into buffer

--- main.h
+++ main.h
@@ -22,4 +22,5 @@ int printf_exclusive_string(va_list val);
 int printf_HEX_aux(unsigned int num);
 int printf_hex_aux(unsigned long int num);
 int printf_pointer(va_list val);
+int printf_rot13(va_list args);
 #endif
